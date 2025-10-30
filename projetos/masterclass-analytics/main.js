6463959e91cc6105efeff1bbf912d7e23ddcf85f
matheusfranciscocruz31-coder <matheusfranciscocruz31@gmail.com>
2025-10-30 14:57:28 -0300

    Delete projetos directory
    remoção da pasta "projetos"

--- projetos/masterclass-analytics/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.faq__question').forEach((button) => {
-    button.addEventListener('click', () => {
-        const item = button.closest('.faq__item');
-        item.classList.toggle('is-open');
-    });
-});
--- projetos/xml-para-excel/xml_para_excel/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Ferramentas para converter NF-e XML em planilhas."""
-
-from .converter import build_workbook, collect_documents
-from .parser import NotaFiscal
-
-__all__ = ["build_workbook", "collect_documents", "NotaFiscal"]
